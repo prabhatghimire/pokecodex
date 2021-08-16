129666067edbe18f66d7398d8ab248e4fbe861b5
Prabhat Ghimire <prabhat@localhost.localdomain>
2021-08-16 23:14:35 +0545

    first commit

---
+++ src/components/PokemonDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokemonDetails = (id, name, image, type) => {
+    return(
+        <div className="thum-container">
+
+        </div>
+    )
+}
+
+export default PokemonDetails
